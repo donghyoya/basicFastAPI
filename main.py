f8cab7d54b2627e28754bb380a51ef111edf597d
donghyoya <dongwooya123@gmail.com>
2024-03-11 19:29:12 +0900

    add redis main

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from domain.user.router import router as user_router
 from domain.multifile.router import router as mf_router
 
+import redis
+
+redis_client = redis.Redis(host='localhost', port=6376, decode_responses=True)
+
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
