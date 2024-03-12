d63acfb50ae7fb7d7e90b272f1360b77c1592740
donghyoya <dongwooya123@gmail.com>
2024-03-12 13:28:48 +0900

    fix multifile curd

--- domain/multifile/crud.py
+++ domain/multifile/crud.py
@@ -1,15 +1,15 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import model, schema
 
-def create_multifile(db: Session, multifile: schemas.MultiFileCreate, user_id: int):
-    db_multifile = models.MultiFile(**multifile.dict(), uid=user_id)
+def create_multifile(db: Session, multifile: schema.MultiFileCreate, user_id: int):
+    db_multifile = model.MultiFile(**multifile.dict(), uid=user_id)
     db.add(db_multifile)
     db.commit()
     db.refresh(db_multifile)
     return db_multifile
 
-def update_multifile(db: Session, mfid: int, multifile: schemas.MultiFileUpdate):
-    db_multifile = db.query(models.MultiFile).filter(models.MultiFile.mfid == mfid).first()
+def update_multifile(db: Session, mfid: int, multifile: schema.MultiFileUpdate):
+    db_multifile = db.query(model.MultiFile).filter(model.MultiFile.mfid == mfid).first()
     if db_multifile:
         update_data = multifile.dict(exclude_unset=True)
         for key, value in update_data.items():
@@ -19,7 +19,7 @@ def update_multifile(db: Session, mfid: int, multifile: schemas.MultiFileUpdate)
     return db_multifile
 
 def delete_multifile(db: Session, mfid: int):
-    db_multifile = db.query(models.MultiFile).filter(models.MultiFile.mfid == mfid).first()
+    db_multifile = db.query(model.MultiFile).filter(model.MultiFile.mfid == mfid).first()
     if db_multifile:
         db.delete(db_multifile)
         db.commit()
