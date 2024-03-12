c7a18c9a9a0a8fc1b3d474e704dfbda404e25156
donghyoya <dongwooya123@gmail.com>
2024-03-12 11:03:43 +0900

    add middleware

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from fastapi import FastAPI
 from domain.user.router import router as user_router
 from domain.multifile.router import router as mf_router
 
-from default.config.corsmiddleware import setup_cors
+from default.middleware.corsmiddleware import setup_cors
+from default.middleware.sessionmiddleware import session_middleware
 
-import redis
-
-redis_client = redis.Redis(host='localhost', port=6376, decode_responses=True)
 
 app = FastAPI()
 
 setup_cors(app)
+app.add_middleware(session_middleware)
 
 app.include_router(user_router, prefix="/user")
 app.include_router(mf_router, prefix="/multifile")
